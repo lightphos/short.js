9cd5ae6accfa9ed00dc5b2a34ed825068a5c0178
lightphos <satishramjee@hotmail.com>
2026-08-14 16:17:19 +0100

    Add shadow DOM example

--- short.js
+++ short.js
@@ -7,3 +7,41 @@ function render(content) {
     document.body.appendChild(div);
     }); // Append the div to the body  
 }
+
+class Short extends HTMLElement {
+    constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    // ===== Markup =====
+    shadow.innerHTML = `
+        <slot name="title">Default Title Here!!!</slot>
+    `;
+
+    fetch('slots.css')
+    .then(res => res.text())
+    .then(css => {
+    const style = document.createElement('style');
+    style.textContent = css;
+    shadow.prepend(style); 
+    });
+    }
+
+    render(content) {
+    this.shadowRoot.querySelector('slot').innerHTML = content;
+    }
+}
+
+function short(content) {
+    document.addEventListener('DOMContentLoaded', () => {
+
+        const el = document.createElement('short-app');
+        el.id = 'short';
+        document.body.appendChild(el);
+
+        customElements.define('short-app', Short);
+
+        const short = document.getElementById('short');
+        short.render(content);
+    })
+}
